f1550d1035e4c3391f8776c2e820110d203b2aaf
Nupoor Patil <95191047+nupoor1@users.noreply.github.com>
2026-01-05 15:52:58 -0500

    Delete app directory

--- app/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white p-4 text-center mt-8">
-      &copy; {new Date().getFullYear()} Nupoor Patil. All rights reserved.
-    </footer>
-  );
-}
--- app/experience/page.tsx
+++
@@ -1,5 +0,0 @@
-import Experience from "../components/experience";
-
-export default function ExperiencePage() {
-  return <Experience />;
-}
